4d2686daf59a0dfb031ff774298e534b62de2234
Elochukwu3 <oguajuromauld5@gmail.com>
2022-07-02 01:36:19 +0100

    final saved

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 //  declarations
-window.alert("Hey input word and Click on the button to Search for words")
+// alert("Hey input word and Click on the button to Search for words")
 let wordContent = document.querySelector("#word");
 let infoText = document.querySelector(".info-text");
 let phoneticSymbol = document.querySelector("#phonetics_symbols");
